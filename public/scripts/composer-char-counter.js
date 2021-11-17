385f79b91acfd9ef6cd5b2378af195422cc4e4e2
github4maninder <maninder.email@gmail.com>
2021-11-16 21:00:18 -0500

    add composer-chat-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  const tweetBox = $('.tweet-box');
+
+  tweetBox.keyup(function () {
+    
+    const counter = $('.counter');
+
+    counter.text(140 - $(this).val().length);
+    if (counter.val() < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', 'black');
+    }
+  });
+});
