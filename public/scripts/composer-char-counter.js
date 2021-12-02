da2af11c4b280b8616300ec023ebe519b36b0ff7
github4maninder <maninder.email@gmail.com>
2021-12-01 19:17:27 -0500

    add gif

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
   const tweetBox = $('.tweet-box');
 
-  tweetBox.keyup(function () {
+  tweetBox.keyup(function() {
     
     const counter = $('.counter');
 
